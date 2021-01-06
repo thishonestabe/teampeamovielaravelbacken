0776a5f73d2df57c48108e4217c681cca70973f3
thishonestabe <marquez.abraham1991@gmail.com>
2021-01-06 15:34:11 +0200

    added login function to login controller

--- app/Http/Controllers/api/v1/LoginController.php
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request) {
+        $login = $request-validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        if( !Auth::attempt($login) ) {
+            return response(['message' => 'Invalid login credentials']);
+        }
+    }
 }

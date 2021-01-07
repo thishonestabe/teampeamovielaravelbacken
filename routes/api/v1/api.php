b655a60a0d5915c7bd804d5d006c1a8453035bcd
thishonestabe <marquez.abraham1991@gmail.com>
2021-01-07 22:28:30 +0200

    final commit

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -29,7 +29,7 @@ Route::prefix('/user')->group( function() {
 Route::prefix('/rental')->group( function() {
     Route::middleware('auth:api')->post('/create', 'api\v1\RentalsController@create');
     Route::middleware('auth:api')->get('/getrentals', 'api\v1\RentalsController@getRentals');
-    Route::middleware('auth:api')->delete('/return', 'api\v1\RentalsController@return');
+    Route::middleware('auth:api')->post('/return', 'api\v1\RentalsController@return');
     
     
 });
